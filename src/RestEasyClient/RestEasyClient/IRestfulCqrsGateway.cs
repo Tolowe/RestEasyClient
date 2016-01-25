466e4d1e5a6c5168abf1cc372d1015d72f1b1546
thomasnlowe@gmail.com <thomasnlowe@gmail.com>
2016-01-24 22:29:07 -0600

    created initial solution. added base interface and started work on default implementation

---
+++ src/RestEasyClient/RestEasyClient/IRestfulCqrsGateway.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace RestEasyClient
+{
+    public interface IRestfulCqrsGateway<T>
+    {
+        void Create<C>(C CreateEntity);
+
+        void Create<C>(string ResourcePath, C CreateEntity);
+
+        IList<T> Get<S>(S Search);
+
+        IList<T> Get<S>(string ResourcePath, S Search);
+
+        T GetById<K>(K Id);
+
+        T GetById<K>(string ResourcePath, K Id);
+
+        void Update<U>(U UpdateEntity);
+
+        void Update<U>(string ResourcePath, U UpdateEntity);
+
+        void Delete<K>(K Id);
+
+        void Delete<K>(string ResourcePath, K Id);
+    }
+}
---
+++ src/RestEasyClient/RestEasyClient/Impl/RestfulCqrsGateway.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RestEasyClient.Impl
+{
+    public class RestfulCqrsGateway<T> : IRestfulCqrsGateway<T>
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestfulCqrsGateway(string ProtocolAndDomain)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(ProtocolAndDomain);
+        }
+
+        public void Create<C>(C CreateEntity)
+        {
+            Create(GetPathFromType(), CreateEntity);
+        }
+
+        public void Create<C>(string ResourcePath, C CreateEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<K>(K Id)
+        {
+            Delete(GetPathFromType(), Id);
+        }
+
+        public void Delete<K>(string ResourcePath, K Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> Get<S>(S Search)
+        {
+            return Get(GetPathFromType(), Search);
+        }
+
+        public IList<T> Get<S>(string ResourcePath, S Search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById<K>(K Id)
+        {
+            return GetById(GetPathFromType(), Id);
+        }
+
+        public T GetById<K>(string ResourcePath, K Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<U>(U UpdateEntity)
+        {
+            Update(GetPathFromType(), UpdateEntity);
+        }
+
+        public void Update<U>(string ResourcePath, U UpdateEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetPathFromType()
+        {
+            return typeof(T).Name;
+        }
+    }
+}
